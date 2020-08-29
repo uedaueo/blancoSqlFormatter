cf38f923a60cac97806ae71acdebe39daadc0beb
tueda <ueo@ueo.co.jp>
2020-08-29 17:37:14 +0900

    2.2.0: Maven build & deploy to ueo's personal repository is now available.

--- blanco/main/blanco/commons/sql/format/valueobject/AbstractBlancoSqlToken.java
+++ blanco/main/blanco/commons/sql/format/valueobject/AbstractBlancoSqlToken.java
@@ -1,6 +1,3 @@
-/*
- * このソースコードは blanco Frameworkにより自動生成されました。
- */
 package blanco.commons.sql.format.valueobject;
 
 /**
@@ -8,98 +5,89 @@ package blanco.commons.sql.format.valueobject;
  */
 public class AbstractBlancoSqlToken {
     /**
-     * フィールド [type]
-     *
-     * 項目の型 [int]<br>
      * トークン種別をあらわします。
+     *
+     * フィールド: [type]。
      */
     private int fType;
 
     /**
-     * フィールド [string]
-     *
-     * 項目の型 [java.lang.String]<br>
      * トークンの実際の文字列をあらわします。
+     *
+     * フィールド: [string]。
      */
     private String fString;
 
     /**
-     * フィールド [pos]
-     *
-     * 項目の型 [int]<br>
-     * 規定値   [-1]<br>
      * 予約語、コメント、値などのトークンの位置をあらわすフィールド。ソース文字列の先頭からの位置をあらわします。値は ゼロ(ZERO)オリジンです。デフォルト値は (-1)で、(-1) の場合には「位置情報に意味がない」ことをあらわします。
+     *
+     * フィールド: [pos]。
+     * デフォルト: [-1]。
      */
     private int fPos = -1;
 
     /**
-     * フィールド [type]のセッターメソッド
+     * フィールド [type] の値を設定します。
      *
-     * 項目の型 [int]<br>
-     * トークン種別をあらわします。
+     * フィールドの説明: [トークン種別をあらわします。]。
      *
-     * @param argType フィールド[type]に格納したい値
+     * @param argType フィールド[type]に設定する値。
      */
     public void setType(final int argType) {
         fType = argType;
     }
 
     /**
-     * フィールド[type]のゲッターメソッド
+     * フィールド [type] の値を取得します。
      *
-     * 項目の型 [int]<br>
-     * トークン種別をあらわします。
+     * フィールドの説明: [トークン種別をあらわします。]。
      *
-     * @return フィールド[type]に格納されている値
+     * @return フィールド[type]から取得した値。
      */
     public int getType() {
         return fType;
     }
 
     /**
-     * フィールド [string]のセッターメソッド
+     * フィールド [string] の値を設定します。
      *
-     * 項目の型 [java.lang.String]<br>
-     * トークンの実際の文字列をあらわします。
+     * フィールドの説明: [トークンの実際の文字列をあらわします。]。
      *
-     * @param argString フィールド[string]に格納したい値
+     * @param argString フィールド[string]に設定する値。
      */
     public void setString(final String argString) {
         fString = argString;
     }
 
     /**
-     * フィールド[string]のゲッターメソッド
+     * フィールド [string] の値を取得します。
      *
-     * 項目の型 [java.lang.String]<br>
-     * トークンの実際の文字列をあらわします。
+     * フィールドの説明: [トークンの実際の文字列をあらわします。]。
      *
-     * @return フィールド[string]に格納されている値
+     * @return フィールド[string]から取得した値。
      */
     public String getString() {
         return fString;
     }
 
     /**
-     * フィールド [pos]のセッターメソッド
+     * フィールド [pos] の値を設定します。
      *
-     * 項目の型 [int]<br>
-     * 予約語、コメント、値などのトークンの位置をあらわすフィールド。ソース文字列の先頭からの位置をあらわします。値は ゼロ(ZERO)オリジンです。デフォルト値は (-1)で、(-1) の場合には「位置情報に意味がない」ことをあらわします。
+     * フィールドの説明: [予約語、コメント、値などのトークンの位置をあらわすフィールド。ソース文字列の先頭からの位置をあらわします。値は ゼロ(ZERO)オリジンです。デフォルト値は (-1)で、(-1) の場合には「位置情報に意味がない」ことをあらわします。]。
      *
-     * @param argPos フィールド[pos]に格納したい値
+     * @param argPos フィールド[pos]に設定する値。
      */
     public void setPos(final int argPos) {
         fPos = argPos;
     }
 
     /**
-     * フィールド[pos]のゲッターメソッド
+     * フィールド [pos] の値を取得します。
      *
-     * 項目の型 [int]<br>
-     * 規定値   [-1]<br>
-     * 予約語、コメント、値などのトークンの位置をあらわすフィールド。ソース文字列の先頭からの位置をあらわします。値は ゼロ(ZERO)オリジンです。デフォルト値は (-1)で、(-1) の場合には「位置情報に意味がない」ことをあらわします。
+     * フィールドの説明: [予約語、コメント、値などのトークンの位置をあらわすフィールド。ソース文字列の先頭からの位置をあらわします。値は ゼロ(ZERO)オリジンです。デフォルト値は (-1)で、(-1) の場合には「位置情報に意味がない」ことをあらわします。]。
+     * デフォルト: [-1]。
      *
-     * @return フィールド[pos]に格納されている値
+     * @return フィールド[pos]から取得した値。
      */
     public int getPos() {
         return fPos;
@@ -108,10 +96,15 @@ public class AbstractBlancoSqlToken {
     /**
      * このバリューオブジェクトの文字列表現を取得します。
      *
-     * オブジェクトのシャロー範囲でしかtoStringされない点に注意して利用してください。
+     * <P>使用上の注意</P>
+     * <UL>
+     * <LI>オブジェクトのシャロー範囲のみ文字列化の処理対象となります。
+     * <LI>オブジェクトが循環参照している場合には、このメソッドは使わないでください。
+     * </UL>
      *
      * @return バリューオブジェクトの文字列表現。
      */
+    @Override
     public String toString() {
         final StringBuffer buf = new StringBuffer();
         buf.append("blanco.commons.sql.format.valueobject.AbstractBlancoSqlToken[");
@@ -120,5 +113,34 @@ public class AbstractBlancoSqlToken {
         buf.append(",pos=" + fPos);
         buf.append("]");
         return buf.toString();
+    }
+
+    /**
+     * このバリューオブジェクトを指定のターゲットに複写します。
+     *
+     * <P>使用上の注意</P>
+     * <UL>
+     * <LI>オブジェクトのシャロー範囲のみ複写処理対象となります。
+     * <LI>オブジェクトが循環参照している場合には、このメソッドは使わないでください。
+     * </UL>
+     *
+     * @param target target value object.
+     */
+    public void copyTo(final AbstractBlancoSqlToken target) {
+        if (target == null) {
+            throw new IllegalArgumentException("Bug: AbstractBlancoSqlToken#copyTo(target): argument 'target' is null");
+        }
+
+        // No needs to copy parent class.
+
+        // Name: fType
+        // Type: int
+        target.fType = this.fType;
+        // Name: fString
+        // Type: java.lang.String
+        target.fString = this.fString;
+        // Name: fPos
+        // Type: int
+        target.fPos = this.fPos;
     }
 }
